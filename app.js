181bd2c6c0804cffb79447165a33282d87b53418
BrendenSeifried <brendenseifried@gmail.com>
2022-01-20 15:40:34 -0800

    forgot to update but, I cought up to the pokedex and have just begun adding a weight parameter and removed audio

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+import { getPokedex } from './fetch.js';
+
 // import functions
-
+console.log('Its ALIVE!');
 // grab DOM elements
-
+const template = document.querySelector('#template');
+const list = document.querySelector('#list');
+const audio = document.querySelector('#audio');
+console.log('test', template, list, audio);
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+async function getPokemon() {
+    const pokemons = await getPokedex();
+    for (let pokemon of pokemons) {
+        const clone = template.content.cloneNode(true);
+        const pokemonName = clone.querySelector('#pokemon');
+        const pokemonImg = clone.querySelector('#image');
+        const defense = clone.querySelector('#defense');
+       // audio.src = '/assets/pokemon.wav';
+       // audio.play();
+
+        pokemonName.textContent = 'Name: ' + pokemon.pokemon;
+        pokemonImg.src = pokemon.url_image;
+        defense.textContent = 'Defense: ' + pokemon.defense;
+        list.append(clone);
+    }
+}
+window.addEventListener('load', async() => {
+    await getPokemon();
+});
